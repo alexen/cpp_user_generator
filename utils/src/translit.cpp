ca89cb14600ec358c4b4e654edb7a34a60afe62c
Enakaev Aleksey <Aleksey.Enakaev@infotecs.ru>
2016-09-22 20:45:28 +0300

    В проект добавлена библиотка для транслитерации libicu + попытка использовать ее

--- utils/src/translit.cpp
+++ utils/src/translit.cpp
@@ -7,10 +7,27 @@
 
 #include <utils/translit.h>
 #include <boost/assert.hpp>
+#include <unicode/translit.h>
+#include <unicode/unistr.h>
+
+#include <iostream>
 
 
 std::string transliterate( const std::string& ruText )
 {
-     BOOST_ASSERT_MSG( false, "Not implemented yet" );
+     UErrorCode ec = U_ZERO_ERROR;
+     StringEnumeration* enumerator = Transliterator::getAvailableIDs( ec );
+     while( const char* id = enumerator->next( nullptr, ec ) )
+     {
+          std::cout << "Transliterate with ID " << id << ":\n";
+          Transliterator* transliterator = Transliterator::createInstance( id, UTRANS_FORWARD, ec );
+          UnicodeString ustr = ruText.c_str();
+          transliterator->transliterate( ustr );
+
+          std::string result;
+          ustr.toUTF8String( result );
+          std::cout << result << "\n";
+     }
+     delete enumerator;
      return "";
 }
