82372981d729aaf0babc5398b70edd1220f0e18e
Enakaev Aleksey <Aleksey.Enakaev@infotecs.ru>
2016-09-24 15:59:38 +0300

    Выпилено использование ICU (от транслитерации я пока отказался)

--- utils/src/translit.cpp
+++ utils/src/translit.cpp
@@ -6,28 +6,9 @@
 ///
 
 #include <utils/translit.h>
-#include <boost/assert.hpp>
-#include <unicode/translit.h>
-#include <unicode/unistr.h>
-
-#include <iostream>
 
 
 std::string transliterate( const std::string& ruText )
 {
-     UErrorCode ec = U_ZERO_ERROR;
-     StringEnumeration* enumerator = Transliterator::getAvailableIDs( ec );
-     while( const char* id = enumerator->next( nullptr, ec ) )
-     {
-          std::cout << "Transliterate with ID " << id << ":\n";
-          Transliterator* transliterator = Transliterator::createInstance( id, UTRANS_FORWARD, ec );
-          UnicodeString ustr = ruText.c_str();
-          transliterator->transliterate( ustr );
-
-          std::string result;
-          ustr.toUTF8String( result );
-          std::cout << result << "\n";
-     }
-     delete enumerator;
      return "";
 }
