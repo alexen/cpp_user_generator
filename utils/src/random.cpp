a19646c4e2c52ea7ca5c5aee9239f3a3f1ad63c8
Enakaev Aleksey <Aleksey.Enakaev@infotecs.ru>
2016-09-28 23:57:39 +0300

    utils: Исправлена ошибка инициализации рандомизатора
    При объявлении глобального ГСЧ как thread_local он инициализируется
    в каждом потоке как отдельный объект и все сгенерированные аккаунты
    получаются одинаковыми. Для исправления внес его внутрь функции и
    объявил как static (это нормальное решение, но только в рамках C++11).

--- utils/src/random.cpp
+++ utils/src/random.cpp
@@ -9,10 +9,9 @@
 #include <boost/random/mersenne_twister.hpp>
 #include <boost/random/uniform_int_distribution.hpp>
 
-thread_local boost::random::mt19937 generator;
-
 int generateRandomInteger( int min, int max )
 {
+     static boost::random::mt19937 generator;
      boost::random::uniform_int_distribution<> dist( min, max );
      return dist( generator );
 }
