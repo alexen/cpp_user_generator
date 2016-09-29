708ba31dad623edfff8b6cd5c7e8ddf07f43f2a0
Enakaev Aleksey <Aleksey.Enakaev@infotecs.ru>
2016-09-29 21:57:18 +0300

    utils/random: Реализована инициализация рандомизатора
    Тестовое использование показало, что рандомизатор необходимо
    инициализировать разными значениями при каждом запуске. time(0) не
    подошло (если запускать программу быстро, пользователи дублируются). Для
    этого была реализована вспомогательная функция aux::nanoseconds().

--- utils/src/random.cpp
+++ utils/src/random.cpp
@@ -6,12 +6,38 @@
 ///
 
 #include <utils/random.h>
+#include <ctime>
+#include <cstring>
+#include <cerrno>
 #include <boost/random/mersenne_twister.hpp>
 #include <boost/random/uniform_int_distribution.hpp>
 
+
+namespace {
+namespace aux {
+
+
+long nanoseconds()
+{
+     timespec tv;
+     if( clock_gettime( CLOCK_REALTIME, &tv ) < 0 )
+     {
+          char errstr[ 128 ] = { 0 };
+          strerror_r( errno, errstr, sizeof( errstr ) );
+          BOOST_THROW_EXCEPTION( std::runtime_error( "clock_gettime failed: "
+               + std::string( errstr ) ) );
+     }
+     return tv.tv_nsec;
+}
+
+
+}  // namespace aux
+}
+
+
 int generateRandomInteger( int min, int max )
 {
-     static boost::random::mt19937 generator;
+     static boost::random::mt19937 generator( aux::nanoseconds() );
      boost::random::uniform_int_distribution<> dist( min, max );
      return dist( generator );
 }
